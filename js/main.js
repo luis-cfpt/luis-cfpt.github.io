befbef1233bd11cc18cefa09ad58fdc26e9a8893
luis-cfpt <114138281+luis-cfpt@users.noreply.github.com>
2023-10-02 11:08:53 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,12 +9,7 @@ if ("serviceWorker" in navigator)
 {
     window.addEventListener("load", () => {
 	navigator.serviceWorker.register("/sw.js");
-	afficheLog.innerHTML = "registerSW - ok!";
     })
-}
-else 
-{
-     afficheLog.innerHTML = "registerSW - ko!"
 }
 
 let listAllCountries = [];
