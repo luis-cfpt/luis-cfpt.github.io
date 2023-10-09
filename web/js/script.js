c17969a1eaf3b74efd1ac2c65a72ae39d6382bd0
luis-cfpt <114138281+luis-cfpt@users.noreply.github.com>
2023-10-09 15:47:11 +0000

    gymchat init

---
+++ web/js/script.js
@@ -0,0 +1,62 @@
+let chatFriends = document.querySelectorAll('.friend');
+let backHome = document.querySelectorAll('.backHome');
+
+let home = document.querySelector('.home-phone');
+let chat = document.querySelector('.chat-phone');
+
+let xStart = 0;
+chat.addEventListener('touchstart', 
+(event) => 
+{
+    xStart = event.touches[0].clientX;
+});  
+
+chat.addEventListener('touchmove', 
+(event) => 
+{
+    let x = event.touches[0].clientX;
+    if (xStart <= 20) 
+    {
+        let diff = x - xStart;
+        console.log('xStart : ', xStart);    
+        
+        if (diff > 0) 
+        {
+            console.log('diff : ', diff);    
+        }        
+    }
+});
+
+for (const friend of chatFriends) 
+{
+    // let xStart = 0;
+    friend.addEventListener('touchstart', 
+    (event) => 
+    {
+        event.target.closest('section').style.left = "-100%";
+        chat.style.left = "0";
+    });   
+
+    // friend.addEventListener('touchmove', 
+    // (event) => 
+    // {
+    //     let x = event.touches[0].clientX;
+    //     let diff = x - xStart;
+    //     console.log('xStart : ', xStart);    
+        
+    //     if (diff < 0) 
+    //     {
+    //         console.log('diff : ', diff);    
+    //     }
+    // });
+}
+
+for (const back of backHome) 
+{
+    back.addEventListener('touchstart', 
+    (event) => 
+    {
+        event.target.closest('section').style.left = "100%";
+        home.style.left = "0";
+    });    
+}
