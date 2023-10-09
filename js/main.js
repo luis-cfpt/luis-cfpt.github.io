030ee6613945bef7596497eef213cce39648a024
luis-cfpt <luis.ds@eduge.ch>
2023-10-09 10:50:20 +0200

    advencement js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 /**
  * Description: Pays du monde entiers
- * Author: LuisDS
- * Version: V1, 25.09.2023
- * File: js/main.js
  */
 
 if ("serviceWorker" in navigator)
 {
-    window.addEventListener("load", () => {
-	navigator.serviceWorker.register("/sw.js");
+    window.addEventListener("load", 
+    () => {
+	    navigator.serviceWorker.register("/sw.js");
     })
 }
 
@@ -16,47 +14,54 @@ let listAllCountries = [];
 
 /**
  * Afficher tous les pays récupérés dans le LocalStorage ou l'API selon le contenu du LocalStorage et de la date de derniere récupération depuis l'API
+ * @returns {void}
  */
 function showCountries() 
 {
-    if (getFromLocalCounties() == null || (new Date(timePassedFromAddCountries()).getMonth() >= 1)) 
+    if (!"cache" in localStorage || (new Date(timePassedFromAddCountries()).getMonth() >= 1)) 
     {
         getCountriesFromAPI()
         .then(countries => 
         {
             listAllCountries = countries;
 
-            addLocalCounties();
-
-            listAllCountries = getFromLocalCounties();
+            addLocalCounties(); 
             
-            /// Affiche les pays dans le HTML
-            for (const country of listAllCountries) 
-            {            
-                document.querySelector('tbody').innerHTML += `<tr class='country-list ${country.translations.fra.common}'><td width="200px">${country.translations.fra.common}</td><td><img src="${country.flags.svg}" width="100px"></td><td width="200px" style="text-align: center;">${country.population}</td></tr>`;
-            }            
-    
+            htmlCountries();
         })
         .catch(err => 
         {
             throw new Error(err);
         });        
     }
-    else
+    else 
     {
-        listAllCountries = getFromLocalCounties();
+        htmlCountries();
+    }
+}
 
-        /// Affiche les pays dans le HTML
-        for (const country of listAllCountries) 
-        {            
-            document.querySelector('tbody').innerHTML += `<tr class='country-list ${country.translations.fra.common}'><td width="200px">${country.translations.fra.common}</td><td><img src="${country.flags.svg}" width="100px"></td><td width="200px" style="text-align: center;">${country.population}</td></tr>`;
-        }
+/**
+ * Affiche les pays dans le html
+ * @returns {void}
+ */
+function htmlCountries()
+{
+    listAllCountries = getFromLocalCounties();
+
+    for (const country of listAllCountries) 
+    {            
+        document.querySelector('tbody').innerHTML += `
+            <tr class='country-list ${country.translations.fra.common}'>
+                <td width="200px">${country.translations.fra.common}</td>
+                <td><img src="${country.flags.svg}" width="100px"></td>
+                <td width="200px" style="text-align: center;">${country.population}</td>
+            </tr>`;
     }
 }
 
 /**
  * Récupère tous les pays depuis l'api
- * @returns countriesObj - Object de tous les pays
+ * @returns {json} - Object de tous les pays
  */
 async function getCountriesFromAPI() 
 {
@@ -64,14 +69,7 @@ async function getCountriesFromAPI()
 
     if (response.ok) 
     {
-        const countriesText = await response.text()
-        try {
-            const countriesObj  = JSON.parse(countriesText);
-            return countriesObj
-        }
-        catch(err) {
-            return err;
-        }
+        return await response.json();
     }
     else 
     {
@@ -81,14 +79,34 @@ async function getCountriesFromAPI()
 }
 
 /**
+ * Remove accents of strings
+ * @param {String} a - words to remove accents
+ * @returns {String} - words modified
+ */
+function strNoAccent(a) {
+    let b="áàâäãåçéèêëíïîìñóòôöõúùûüýÁÀÂÄÃÅÇÉÈÊËÍÏÎÌÑÓÒÔÖÕÚÙÛÜÝ",
+        c="aaaaaaceeeeiiiinooooouuuuyAAAAAACEEEEIIIINOOOOOUUUUY",
+        d="";
+
+    for(let i = 0, j = a.length; i < j; i++) 
+    {
+      let e = a.substr(i, 1);
+      d += (b.indexOf(e) !== -1) ? c.substr(b.indexOf(e), 1) : e;
+    }
+
+    return d;
+  }
+
+/**
  * Ajout les pays en les classant par ordre alphabétique dans le LocalStorage
+ * @returns {void}
  */
 function addLocalCounties()
 {
     listAllCountries.sort((a, b) => {
-        let fa = a.translations.fra.common.toLowerCase(),
-            fb = b.translations.fra.common.toLowerCase();
-    
+        let fa = strNoAccent(a.translations.fra.common.toLowerCase()),
+            fb = strNoAccent(b.translations.fra.common.toLowerCase());
+
         if (fa < fb) {
             return -1;
         }
@@ -98,19 +116,23 @@ function addLocalCounties()
         return 0;
     });  
 
-    localStorage.setItem('countries', JSON.stringify(listAllCountries));
+    localStorage.setItem('cache', JSON.stringify(listAllCountries));
     localStorage.setItem('timeAddCountries', Date.now())
 }
 
 /**
  * Récupère tous les pays situés dans le LocalStorage 
- * @returns Object of all countries
+ * @returns {json} Object of all countries
  */
 function getFromLocalCounties()
 {
-    return JSON.parse(localStorage.getItem('countries'));
+    return JSON.parse(localStorage.getItem('cache'));
 }
 
+/**
+ * Fait la difference entre la date présent avec la date de recupération des données
+ * @returns {int} La différence entre l adate présent 
+ */
 function timePassedFromAddCountries()
 {
     return Date.now() - localStorage.getItem('timeAddCountries');
