6f42d33174ebddae15f100435aa29b46d6a23949
luis-cfpt <114138281+luis-cfpt@users.noreply.github.com>
2023-10-16 12:59:54 +0000

    fluid

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ let backHome = document.querySelectorAll('.backHome');
 
 let home = document.querySelector('.home-phone');
 let chat = document.querySelector('.chat-phone');
+let dragChat = document.querySelector('.text-chat');
 
 window.addEventListener('resize', e => {
     // console.log(e.currentTarget);
@@ -11,13 +12,13 @@ window.addEventListener('resize', e => {
 });
 
 let xStart = 0;
-chat.addEventListener('touchstart', 
+dragChat.addEventListener('touchstart', 
 (event) => 
 {
     xStart = event.touches[0].clientX;
 });  
 
-chat.addEventListener('touchmove', 
+dragChat.addEventListener('touchmove', 
 (event) => 
 {
     let x = event.touches[0].clientX;
@@ -28,10 +29,38 @@ chat.addEventListener('touchmove',
         
         if (diff > 0) 
         {
-            console.log('diff : ', diff);    
+            chat.style.transition = "";
+            chat.style.left = diff + 'px';              
         }        
     }
 });
+
+dragChat.addEventListener('touchend', 
+(event) => 
+{
+    console.log(event);
+    xEnd = event.changedTouches[0].clientX;
+    chat.style.transition = "left .1s linear";
+
+    if (xStart <= 20) 
+    {     
+        
+        if (xEnd < window.innerWidth / 2.5) 
+        {
+            chat.style.left = "0";
+        }
+        else 
+        {
+            chat.style.left = "100%";
+        }                
+
+        if (chat.style.left != "0" && chat.style.left != "100%") 
+        {
+            chat.style.left = "0";
+        }
+    }    
+
+});  
 
 for (const friend of chatFriends) 
 {
@@ -43,18 +72,55 @@ for (const friend of chatFriends)
         chat.style.left = "0";
     });   
 
-    // friend.addEventListener('touchmove', 
-    // (event) => 
-    // {
-    //     let x = event.touches[0].clientX;
-    //     let diff = x - xStart;
-    //     console.log('xStart : ', xStart);    
-        
-    //     if (diff < 0) 
-    //     {
-    //         console.log('diff : ', diff);    
-    //     }
-    // });
+    let xGoStart = 0;
+    friend.addEventListener('touchstart', 
+    (event) => 
+    {
+        xGoStart = event.touches[0].clientX;
+    });  
+
+    friend.addEventListener('touchmove', 
+    (event) => 
+    {
+        let x = event.touches[0].clientX;
+        if (xGoStart >= window.innerWidth - 50) 
+        {
+            let diff = x - xGoStart;   
+            console.log(diff);
+            
+            if (diff < 0) 
+            {
+                chat.style.transition = "";
+                chat.style.left = "calc(100% - " + Math.abs(diff) + 'px)';              
+            }        
+        }
+    });
+
+    friend.addEventListener('touchend', 
+    (event) => 
+    {
+        xEnd = event.changedTouches[0].clientX;
+        chat.style.transition = "left .1s linear";
+
+        if (xGoStart >= window.innerWidth - 50) 
+        {     
+            
+            if (xEnd <= window.innerWidth / 2) 
+            {
+                chat.style.left = "0";
+            }
+            else 
+            {
+                chat.style.left = "100%";
+            }
+
+            if (chat.style.left != "0" && chat.style.left != "100%") 
+            {
+                chat.style.left = "0";
+            }
+        }    
+
+    }); 
 }
 
 for (const back of backHome) 
